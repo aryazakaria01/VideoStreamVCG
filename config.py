998f2b348e482d1eccd5e2b2ac73801e4c7090e6
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-17 16:19:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2021 By VideoStreamVCG
+
+import os
 from os import path, getenv
 from dotenv import load_dotenv
 
@@ -5,13 +8,19 @@ if path.exists("local.env"):
     load_dotenv("local.env")
 
 load_dotenv()
-admins = {}
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-API_ID = int(getenv("API_ID", "6"))
-API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION_NAME = getenv("SESSION_NAME", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+
+class Cyber(object):
+        admins = {}
+        BOT_TOKEN = getenv("BOT_TOKEN", None)
+        CHANNEL = int(os.getenv('CHANNEL', 123456))
+        API_ID = int(getenv("API_ID", "6"))
+        API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+        SESSION_NAME = getenv("SESSION_NAME", None)
+        DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+        SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+        ASSISTANT_NAME = getenv("ASSISTANT_NAME", "JIK")
+        BOT_USERNAME = getenv("BOT_USERNAME", "HH")
+        COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+        CHANNEL_NAME = getenv("CHANNEL_NAME", "PP")
+        GROUP_NAME = getenv("GROUP_NAME", "MK")
+        OWNER_NAME = getenv("OWNER_NAME", "BDYM")
