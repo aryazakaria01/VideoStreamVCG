b5a6f29017a308625ac376db5dbfe2d3f7fa9772
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-17 16:46:06 +0700

    Update ytsearch.py

--- VideoStream/ytsearch.py
+++ VideoStream/ytsearch.py
@@ -1,24 +1,15 @@
-import logging
-
 from pyrogram import Client as app
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
-
-from config import BOT_USERNAME
+from config import Cyber
 from helpers.filters import command
 
-# logging
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-@app.on_message(command(["search", f"search@{BOT_USERNAME}"]))
+@app.on_message(command(["search", f"search@{Cyber.BOT_USERNAME}"]))
 async def ytsearch(_, message: Message):
     m = await message.reply_text("🔎 **Searching url...**")
     try:
         if len(message.command) < 2:
-            await message.reply_text("`/search` Needs an argument!")
+            await message.reply_text("`/search` needs an argument!")
             return
         query = message.text.split(None, 1)[1]
         results = YoutubeSearch(query, max_results=5).to_dict()
